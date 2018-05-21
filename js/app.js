45bef9e91a9dafd433109cacbaaf010e772f5ecf
Michelle Austin <mchllaustin@gmail.com>
2018-05-21 17:53:14 +0100

    finished mvp

--- js/app.js
+++ js/app.js
@@ -1,2 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.dir("here we go");
+
+    const form = document.querySelector('#form');
+    form.addEventListener('submit', handleFormEntered);
+
+  });
+
+  const handleFormEntered = function(event){
+    event.preventDefault();
+    const formResult = document.querySelector('#form-result');
+    formResult.textContent = `| Title: ${this.title.value} | Author: ${this.author.value} | Category: ${this.category.value} | Genre: ${this.genre.value}`;
+  };
